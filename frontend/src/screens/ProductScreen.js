892d4dde3c66d6624839c39c3285577c3082e578
Quzeem <quzeemagbaje@gmail.com>
2020-11-27 13:11:18 +0100

    React Router and React Router Bootstrap
    - Implement react router for homescreen and productscreen
    - Replace anchor tags with Link tags
    - wrap links in Header with LinkContainer

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductScreen = () => {
+  return (
+    <div>
+      <h1>Each product page</h1>
+      <h2>Order now!</h2>
+    </div>
+  )
+}
+
+export default ProductScreen
