73219b92bf0f02e90be39a6818d97a86faea4ceb
Quzeem <quzeemagbaje@gmail.com>
2020-12-23 10:33:13 +0100

    Add product details reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers'
 
 // Turns an object whose values are different reducer functions, into a single reducer function.
-const reducer = combineReducers({ productList: productListReducer })
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+})
 
 // Anything we want to load when the redux store loads can be specify here
 const initialState = {}
